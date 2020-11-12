e234865dbd00e110109d7219e6d74de01cbff750
cxq1915495507 <cxq1915495507@yahoo.co.jp>
2020-11-12 15:13:31 +0900

    a

---
+++ quic_suite_test.go
@@ -0,0 +1,28 @@
+package quic
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestQuicGo(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "QUIC Suite")
+}
+
+var mockCtrl *gomock.Controller
+
+var _ = BeforeEach(func() {
+	mockCtrl = gomock.NewController(GinkgoT())
+
+	// reset the sync.Once
+	connMuxerOnce = *new(sync.Once)
+})
+
+var _ = AfterEach(func() {
+	mockCtrl.Finish()
+})
